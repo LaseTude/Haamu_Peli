b963a3bffe2dd43a5281b7d24d0890d4a6833725
LaseTude <lassentunnusgmail.com>
2025-05-06 17:35:28 +0300

    kahdestoista commit

--- public/script.js
+++ public/script.js
@@ -88,6 +88,9 @@ function drawBoard(board){
             else if(board[y][x] == 'P'){
                 cell.classList.add('player');
             }
+            else if(board[y][x] == 'H'){
+                cell.classList.add('hornmonster');
+            }
             gameBoard.appendChild(cell);
         }
     }
